a9b041198a829b40d64c77ec114959a09aa6280a
Anthony Sherbondy <ts1235813@gmail.com>
2014-03-16 14:00:00 -0700

    Initial project creation

---
+++ RottenMoviesApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RottenMoviesApp
+//
+//  Created by Anthony Sherbondy on 3/16/14.
+//  Copyright (c) 2014 Anthony Sherbondy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RottenMoviesApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RottenMoviesApp
+//
+//  Created by Anthony Sherbondy on 3/16/14.
+//  Copyright (c) 2014 Anthony Sherbondy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
