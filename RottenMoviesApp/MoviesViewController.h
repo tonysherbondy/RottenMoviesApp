28650de0b93017fa2bd449548df5309b80f49ff5
Anthony Sherbondy <ts1235813@gmail.com>
2014-03-16 14:33:59 -0700

    Can communicate with API and fill table title

---
+++ RottenMoviesApp/MoviesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoviesViewController.h
+//  RottenMoviesApp
+//
+//  Created by Anthony Sherbondy on 3/16/14.
+//  Copyright (c) 2014 Anthony Sherbondy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoviesViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ RottenMoviesApp/MoviesViewController.m
@@ -0,0 +1,67 @@
+//
+//  MoviesViewController.m
+//  RottenMoviesApp
+//
+//  Created by Anthony Sherbondy on 3/16/14.
+//  Copyright (c) 2014 Anthony Sherbondy. All rights reserved.
+//
+
+#import "MoviesViewController.h"
+
+@interface MoviesViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSArray *movies;
+@end
+
+@implementation MoviesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        [self loadMovies];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    NSDictionary *movie = self.movies[indexPath.row];
+    cell.textLabel.text = movie[@"title"];
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.movies.count;
+}
+
+#pragma mark - Private
+- (void) loadMovies {
+    NSString *url = @"http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=g9au4hv6khv6wzvzgt55gpqs";
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        id object = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        self.movies = object[@"movies"];
+        NSLog(@"%@", self.movies);
+        [self.tableView reloadData];
+    }];
+}
+
+@end
